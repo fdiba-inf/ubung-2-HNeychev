7173faecab1d7b79128587f69fa89da954c751ff
HNeychev <>
2021-10-25 13:54:20 +0000

    popravka 2

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -6,10 +6,10 @@ public class Circle {
 
     public static void main(String[] args) {
        Scanner input = new Scanner(System.in);
-        System.out.println("Enter r :");
+        System.out.println("Enter r : ");
         double number = input.nextDouble ();
         double pi=Math.PI;
-        System.out.println("Circumference :"+ (2*pi)*number);
-        System.out.println("Area :"+ pi*(number*number));
+        System.out.println("Circumference : "+ (2*pi)*number);
+        System.out.println("Area : "+ pi*(number*number));
     }
 }
